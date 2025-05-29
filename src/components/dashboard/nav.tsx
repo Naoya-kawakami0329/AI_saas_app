7d1502e7bd74cd77e39889ce4e6b7ee15e2e4dc9
Naoya-kawakami0329 <naoya.kawakami@arteryex.biz>
2025-05-29 23:52:45 +0900

    shadcnでナビゲーションUI作成

--- src/components/dashboard/nav.tsx
+++ src/components/dashboard/nav.tsx
@@ -1,9 +1,32 @@
+'use client'
+
+import { usePathname } from 'next/navigation'
+import { navItems } from '@/config/nav'
 import React from 'react'
+import { Button } from '../ui/button'
+import Link from 'next/link'
+import { cn } from '@/lib/utils'
 
 const DashboardNav = () => {
+  const pathname = usePathname()
   return (
-    <nav>
-      {/* nav items*/}
+    <nav className="grid gap-2 items-start">
+      {navItems.map((item) => (
+        <Button
+          key={item.href}
+          variant={pathname === item.href ? 'secondary' : 'ghost'}
+          className={cn(
+            'justify-start',
+            pathname === item.href && 'bg-amber-200 text-primary'
+          )}
+          asChild
+        >
+          <Link href={item.href}>
+            {item.icon && <item.icon className="mr-2 h-4 w-4" />}
+            {item.title}
+          </Link>
+        </Button>
+      ))}
     </nav>
   )
 }
