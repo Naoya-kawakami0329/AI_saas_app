fbbede7856b072156157d760a17ac54407401314
Naoya-kawakami0329 <naoya.kawakami@arteryex.biz>
2025-06-16 20:42:46 +0900

    Stable Diffusion API

--- src/app/api/generate-image/route.ts
+++ src/app/api/generate-image/route.ts
@@ -1,14 +1,39 @@
 import { NextRequest, NextResponse } from "next/server";
+import axios from "axios";
+import FormData from "form-data";
 
 export async function POST(req: NextRequest) {
-   try {
-    const { prompt } = await req.json();
+    try {
+        const { prompt } = await req.json();
 
-    const response = await fetch(`https://api.openai.com/v1/images/generations`, {
-        method: "POST",
-    });
-   } catch (error) {
-    console.error(error);
-    return NextResponse.json({ error: "Failed to generate image" }, { status: 500 });
-   }
+        const payload = {
+            prompt: prompt,
+            output_format: "png"
+        };
+
+        const response = await axios.postForm(
+            `https://api.stability.ai/v2beta/stable-image/generate/core`,
+            axios.toFormData(payload, new FormData()),
+            {
+                validateStatus: undefined,
+                responseType: "arraybuffer",
+                headers: { 
+                    Authorization: `Bearer ${process.env.STABILITY_API_KEY}`, 
+                    Accept: "image/*" 
+                },
+            },
+        );
+
+        if (response.status !== 200) {
+            throw new Error(`${response.status}: ${response.data.toString()}`);
+        }
+
+        console.log(response.data);
+    } catch (error) {
+        console.error(error);
+        return NextResponse.json(
+            { error: "画像の生成に失敗しました" }, 
+            { status: 500 }
+        );
+    }
 }
